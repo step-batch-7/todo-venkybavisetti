4603a47b43e9d5b644db3fa550445dd82860a45e
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2020-02-03 16:41:02 +0530

    refactored app.js file in lib

--- public/js/index.js
+++ public/js/index.js
@@ -7,26 +7,22 @@ const postHttpMsg = function(url, callback, newTitle) {
   };
   req.open('POST', url);
   req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  req.send(`title=${newTitle}`);
+  req.send(newTitle);
 };
+
 const callback = function(text) {
   const container = document.getElementById('todoList');
   console.log(container);
   const json = JSON.parse(text);
-  //   json.forEach(todo => {
-  //     const task = document.createElement('div');
-  //     task.classList.add('todoBox');
-  //     task.textContent = todo.title;
-  //     container.appendChild(task);
-  //   });
   const task = document.createElement('div');
   task.classList.add('todoBox');
   task.textContent = json.title;
   container.insertBefore(task, container.childNodes[0]);
 };
+
 const addTitle = function() {
   const newTitleBox = document.querySelector('#title');
   const newTitle = newTitleBox.value;
   newTitleBox.value = '';
-  postHttpMsg('/addTitle', callback, newTitle);
+  postHttpMsg('/addTitle', callback, `title=${newTitle}`);
 };
