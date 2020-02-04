dbb8acb2774c9290aa2a759da3b8f65e31aa039c
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2020-02-04 12:30:53 +0530

    sending json to the front end

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-const postHttpMsg = function(url, callback, newTitle) {
+const postHttpMsg = function(url, callback, message) {
   const req = new XMLHttpRequest();
   req.onload = function() {
     if (this.status == '200') {
@@ -7,7 +7,7 @@ const postHttpMsg = function(url, callback, newTitle) {
   };
   req.open('POST', url);
   req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  req.send(newTitle);
+  req.send(message);
 };
 
 const postCallback = function(text) {
@@ -16,15 +16,31 @@ const postCallback = function(text) {
   container.innerHTML = text;
 };
 
+const createTitles = function(task) {
+  const todoBox = document.createElement('div');
+  todoBox.classList.add('todoBox');
+  todoBox.textContent = task.title;
+  return todoBox;
+};
+
+const generateTodoTasks = function(text) {
+  const todoTitleContainer = document.getElementById('todoList');
+  todoTitleContainer.innerHTML = '';
+  const todoTasksJson = JSON.parse(text);
+  console.log(todoTasksJson);
+  const todoTitles = todoTasksJson.map(createTitles);
+  todoTitles.forEach(task => todoTitleContainer.appendChild(task));
+};
+
 const addTitle = function() {
   const newTitleBox = document.querySelector('#title');
   const newTitle = newTitleBox.value;
   newTitleBox.value = '';
-  postHttpMsg('/addTodoBox', postCallback, `title=${newTitle}`);
+  postHttpMsg('/addTodoBox', generateTodoTasks, `title=${newTitle}`);
 };
 
 const removeTodoTitle = function() {
-  const [, , todoBox] = event.path;
+  const [, todoBox] = event.path;
   const todoBoxId = todoBox.id;
   postHttpMsg('/removeTodoBox', postCallback, `id=${todoBoxId}`);
 };
