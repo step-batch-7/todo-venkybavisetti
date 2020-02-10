5f8d53d32334b8a742695dee7666429bf8d6081f
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2020-02-10 10:29:16 +0530

    implemented test on the new task adding to the todo list

--- test/handlersTest.js
+++ test/handlersTest.js
@@ -1,7 +1,9 @@
 'use strict';
 const request = require('supertest');
+const sinon = require('sinon');
 const { app } = require('../lib/handlers');
 const App = app.serve.bind(app);
+const fs = require('fs');
 
 describe('FILE NOT FOUND', () => {
   it('Should give file not found if file not exist', done => {
@@ -37,9 +39,34 @@ describe('GET Home page', () => {
   });
   it('should get the path /css/homeStyles.css', done => {
     request(App)
-      .get('/css/index.css')
+      .get('/css/homeStyle.css')
       .set('Accept', '*/*')
       .expect(200)
       .expect('Content-Type', 'text/css', done);
+  });
+  it('should get the path /css/animations.css', done => {
+    request(App)
+      .get('/css/animations.css')
+      .set('Accept', '*/*')
+      .expect(200)
+      .expect('Content-Type', 'text/css', done);
+  });
+});
+
+describe('POST', () => {
+  beforeEach(() => {
+    sinon.replace(fs, 'writeFileSync', () => {});
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('should post the title to add', done => {
+    request(App)
+      .post('/addTask')
+      .send('title=newTodo')
+      .expect(200)
+      .expect('Content-Type', 'application/json', done);
   });
 });
